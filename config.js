84bed87235d6299f6797fd73d404a331d32ada3c
Trongloi96 <85351241+Trongloi96@users.noreply.github.com>
2021-06-04 20:38:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Thanh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,11 +8,12 @@ const CONFIG = {
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <3',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Anh trai mưa :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại là người anh yêu <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Còn gì nữa mà không nhắn tin cho anh đi nè bé <3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/chaunhat99' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/chaunhat99'
 }
